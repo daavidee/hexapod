7f4245a0b2c7ef6ce74e5c5ccb35b04826cbee8e
spydeee <majik.poof@gmail.com>
2014-09-26 02:44:02 -0400

    update

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# TODO
-#
-# return to initial position after no movement instead of staying mid-way through motion
-# on start of walking, make sure movements are smooth and feet aren't dragging on ground to go to initial offset positions
-# change standing to be smooth and to use combo femur/tibia movement instead of just femur (extend servo life)
-
 import time
 from hexapodMotion import hexapodMotion
 
@@ -11,11 +5,7 @@ motion = hexapodMotion()
 
 walkSpeed = 1
 
-
-
 motion.testServoOffsets()
-
-
 
 '''
 # stand up and wait for input
