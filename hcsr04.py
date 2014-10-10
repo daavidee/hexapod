eaad90c668fad836c2b52440ec541df0be9f1f5f
spydeee <majik.poof@gmail.com>
2014-10-09 22:33:38 -0400

    update

--- hcsr04.py
+++ hcsr04.py
@@ -10,16 +10,20 @@ class hcsr04:
 		self.address = i2cAddress # address of the micro
 		print "initialized connection to ATmega328P at address " + str(self.address) + " on i2c bus number " + str(busAddress)
 	def ping(self):
-		self.bus.write_byte_data(self.address, 1, 10) # write to register 1 the value 10 to start a ping. data is unused and can be anything
-		for i in range(1, 20):
-			time.sleep(0.01) # don't bother it too quickly
-			tmp = self.bus.read_byte(self.address)
-			if tmp != 0: return tmp
-			elif i == 20: return -1
+		try:
+			self.bus.write_byte_data(self.address, 1, 10) # write to register 1 the value 10 to start a ping. data is unused and can be anything
+			for i in range(1, 10):
+				time.sleep(0.05) # don't bother it too quickly
+				tmp = self.bus.read_byte(self.address)
+				if tmp > 0: return tmp
+				if tmp == -1: return tmp
+			return -1
+		except:
+			return self.currentDistance
 	def updateObjectDistance(self):
 		while True:
 			self.currentDistance = self.ping()
-			time.sleep(0.2)
+			time.sleep(0.05)
 	def beginIntervalPinging(self):
 		thread.start_new_thread(self.updateObjectDistance, ())
 	def getPingDistance(self):
--- main.py
+++ main.py
@@ -1,39 +1,29 @@
-# TODO
-# put parameter loop in hexapodMotion class instead 
-
 import time
-import math
-import copy
 from hexapodMotion import hexapodMotion
 from sixAxis import sixAxis
 from hcsr04 import hcsr04
+import os
 
 # initialize classes
-#motion = hexapodMotion()
+hexapod = hexapodMotion()
 sixAxis = sixAxis()
-hcsr04 = hcsr04(1, 0x04) # i2c bus #1 and i2c device address 0x04. this connects to the ATmega328P
+hcsr04 = hcsr04(1, 4) # i2c bus #1 and i2c device address 0x04. this connects to the ATmega328P
 
-# initial variables
-minWalkScale = 5 # this scales the r3_vertical value to affect the walk speed
-maxWalkScale = 100
-curWalkValue = 0 # the r3_vertical value
 
 # vars used in follow-mode
-followModeEnable = False
+bFollowModeEnabled = False
 minFollowDistance = 5 # values in cm. these are the values to maintain in 'follow-mode' between the hexapod and an object in front using the ultrasonic sensor
 maxFollowDistance = 50
 followDistance = maxFollowDistance / 2 # the follow-mode distance to maintain
-followModeWalkValue = 0.5 # value between 0-1 to affect the speed, higher being faster
+followModeWalkSpeed = 0.3 # value between 0-1 to affect the speed, higher being faster
+
+
+# tmp variable to move the ping servo
+pingServoValue = 350
 
 
 # supporting functions
 
-# modify the walking speed based on input from the right (r3) analog stick
-def calcWalkSpeed():
-	tmp = int(math.fabs(curWalkValue * maxWalkScale))
-	if tmp > maxWalkScale: tmp == maxWalkScale
-	if tmp == 0: tmp = minWalkScale
-	return (maxWalkScale - tmp)
 
 # modify the follow-mode ping distance to maintain. this changes based on dpad presses
 def calcfollowDistance(direction):
@@ -43,15 +33,20 @@ def calcfollowDistance(direction):
 
 
 # stand up
-#time.sleep(3)
-#motion.stand()
+hexapod.stand()
 print "hexapod standing up"
+time.sleep(1)
 
 
-# main parameter loop
+# begin pinging. interval is determined in sixAxis class. defaults to 200ms
+hcsr04.beginIntervalPinging()
+time.sleep(0.2)
 
-x = 0 # parameter variable
-hcsr04.beginIntervalPinging() # begin pinging. interval is determined in sixAxis class. defaults to 200ms
+# debugging
+#hexapod.walkSpeed = 0.3
+#hexapod.testMode = True
+
+# main loop
 while True:
 	# monitor for buttonpresses on sixaxis
 	events = sixAxis.getEvents()
@@ -62,39 +57,41 @@ while True:
 		if buttonName == 'dpad_down': 
 			calcfollowDistance(-1)
 			print followDistance
-		if buttonName == 'select':
-			followModeEnable = not followModeEnable
-			# in follow mode, make it walk faster
-			if followModeEnable: curWalkValue = followModeWalkValue
-			else: curWalkValue = 0
-				
-		if buttonName == 'r3_vertical': curWalkValue = events[buttonName]['axisValue']
+		if buttonName == 'dpad_left':
+			pingServoValue += 5
+			hexapod.pwmL.setPWM(1, 0, pingServoValue)
+			print pingServoValue
+		if buttonName == 'dpad_right':
+			pingServoValue -= 5
+			hexapod.pwmL.setPWM(1, 0, pingServoValue)
+			print pingServoValue
+		if buttonName == 'start':
+			os.system('flite -voice kalit -t "i am ah robot"&')
+		if buttonName == 'select':		
+			bFollowModeEnabled = not bFollowModeEnabled
+			# in follow mode, make it walk at a specific speed
+			if bFollowModeEnabled:
+				print "follow mode enabled"
+				hexapod.walkSpeed = followModeWalkSpeed
+			else:
+				print "follow mode disabled"
+				hexapod.walkSpeed = 0
+			
+		
+		if buttonName == 'r3_vertical' and bFollowModeEnabled == False: hexapod.walkSpeed = events[buttonName]['axisValue']
 		
 		
 	# check if follow-mode is enabled
-	if followModeEnable == True:
+	if bFollowModeEnabled == True:
 		objectDistance = hcsr04.getPingDistance()
 		print objectDistance
-		if objectDistance == -1: print objectDistance
-		if objectDistance - followDistance > 3:
+		if objectDistance - followDistance > 5:
 			print "walking forward"
-			x += 1
-		if objectDistance - followDistance < -3:
+			hexapod.walkSpeed = followModeWalkSpeed 
+			hexapod.walk()
+		if objectDistance - followDistance < -5:
 			print "walking backward"
-			x -= 1
+			hexapod.walkSpeed = -followModeWalkSpeed 
+			hexapod.walk()
 	else:
-		if curWalkValue > 0:
-			print "walking forward"
-			print x
-			x += 1
-		elif curWalkValue < 0:
-			print "walking backward"
-			print x
-			x -= 1
-		
-	#print x
-	#motion.walk(x)
-	time.sleep(0.001 * calcWalkSpeed())
-	
-
-#motion.testServoOffsets()
+			hexapod.walk()
